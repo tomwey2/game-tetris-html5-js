08ccdb4460a3e38ca57ad7afbb5e99ee00d79110
Thomas <tomwey2@gmail.com>
2025-01-02 10:52:29 +0400

    removed unused function

--- js/utils.js
+++ js/utils.js
@@ -1,9 +1,5 @@
 "use strict";
 
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
-}
-
 function drawFillRect(x, y, width, height, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, width, height);
